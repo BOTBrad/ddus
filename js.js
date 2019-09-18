9a8cf1ccadd003838026dc62c7ebb9538e96602b
christian w <i@hate.coffee>
2019-09-17 22:03:18 -0400

    collaborative box movement

--- js.js
+++ js.js
@@ -1,20 +1,40 @@
 let peer = new Peer();
 let conn = null;
 
+let events = [];
+
 peer.on('error', (error) => console.error(error));
+
+const sortEvents = () => {
+  events.sort((a, b) => {
+    const timediff = a.timestamp - b.timestamp;
+    if (timediff !== 0 ) {
+      return timediff;
+    }
+
+    return a.user < b.user;
+  });
+}
 
 const greet = () => {
   if (!conn) {
     return;
   }
 
+  start = Date.now();
   conn.send('connected');
 };
 
 const dataDiv = document.getElementById('data');
 
 let handleData = (data) => {
-  dataDiv.appendChild(document.createTextNode(''+data));
+  const {timestamp, value, user} = data;
+  if (timestamp) {
+    events.push({timestamp:timestamp, value:value, user: user});
+    sortEvents();
+  } else {
+    console.log(data);
+  }
 };
 
 peer.on('open', () => {
@@ -40,10 +60,56 @@ const otherSubmit = () => {
   setupConnection(peer.connect(document.getElementById('other-id').value));
 };
 
+const keys = {
+  'a': -1,
+  'd': 1,
+};
+
 document.onkeypress = (k) => {
-  if (conn) {
-    key = k.key;
-    handleData(key)
-    conn.send(key);
+  const dir = keys[k.key];
+  if (!conn || !dir) {
+    return;
   }
-}; 
+
+  const evt = {
+    timestamp: Date.now(),
+    value: dir,
+    user: peer.id,
+  }
+
+  handleData(evt);
+  conn.send(evt);
+};
+
+const getValue = () => {
+  events.push({timestamp: Date.now()});
+  let x = 500;
+  let lastEvt;
+  events.forEach((evt) => {
+    if (!lastEvt) {
+      lastEvt = evt;
+      return;
+    }
+
+    let vel = lastEvt.value * (evt.timestamp - lastEvt.timestamp) / 40;
+    x += vel;
+
+    lastEvt = evt;
+  });
+  events.pop();
+
+  return x;
+};
+
+let box = document.getElementById('box');
+
+const repeat = () => {
+  box.style.width = '20px';
+  box.style.height = '20px';
+  box.style.backgroundColor = 'red';
+  box.style.marginLeft = getValue() + 'px';
+
+  window.requestAnimationFrame(repeat);
+}
+
+repeat();
